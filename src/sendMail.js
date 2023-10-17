8b9a685e188c2f1fc9a2e9094e003bdf987f97b4
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2023-10-16 18:20:39 -0600

    Update sendMail.js 2.0

--- src/sendMail.js
+++ src/sendMail.js
@@ -3,8 +3,8 @@ const transporter = require('./transporter');
 const mail = {};
 
 mail.sendTextMail = async(to, subjecto, text, successCallback, errorCallBack) => {
-    const html = `<h5>Hola, tu contraseña para iniciar sesion es:<br><br>${text}</h5>
-     <button><a href="https://dash-chipsi.vercel.app/login/${to}/${text}"><h3>Redireccionar</h3></a></button>
+    const html = `<h2>Hola, tu contraseña para iniciar sesion es:<br><br>${text}</h2>
+     <button><a href="https://dash-chipsi.vercel.app/login/${to}/${text}"><h5>Redireccionar</h5></a></button>
     `
     
     const correo = {
